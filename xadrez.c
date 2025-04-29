a081d63e6818acd0110e161d943f96fd22df6f0b
josueferreira2025 <josuegf2017@gmail.com>
2025-04-29 23:29:54 +0000

    jogo concluido no modo novato

--- xadrez.c
+++ xadrez.c
@@ -2,12 +2,14 @@
 
 int main() {
     // Simulação do movimento da Torre (for loop)
+    //Torre, movimente-se 5 casas para a direita
     printf("Movimento da Torre:\n");
     for (int i = 1; i <= 5; i++) {
         printf("Direita %d\n", i);
     }
 
     // Simulação do movimento do Bispo (while loop)
+    //Bispo, movimente-se 5 casas na diagonal pracima e a direita
     printf("\nMovimento do Bispo:\n");
     int j = 1;
     while (j <= 5) {
@@ -16,12 +18,13 @@ int main() {
     }
 
     // Simulação do movimento da Rainha (do-while loop)
+    //Rainha, aplique xeque mate movimentando oito casas para a esquerda
     printf("\nMovimento da Rainha:\n");
     int k = 1;
     do {
         printf("Esquerda %d\n", k);
         k++;
-    } while (k <= 8);
+    } while (k <= 8); 
 
     return 0;
 }
